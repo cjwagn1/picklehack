1d2558fea06734f7ec3e14c0e352622ce1477b1a
Carter Wagner <37089795+cjwagn1@users.noreply.github.com>
2020-11-13 20:19:45 -0600

    Fix (#1)
    * npm fix
    
    * test commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,5 +5,5 @@ import styled, { AnyStyledComponent } from "styled-components";
 const Example: AnyStyledComponent = styled.div``;
 
 export default () => {
-  return <Example>This is an example component</Example>;
+	return <Example>This is an exampl component</Example>;
 };
