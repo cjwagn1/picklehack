a70cce4857caee4b20ddfbbd08b4c6c74c5d94a2
dgnn5 <mrinvent0r@icloud.com>
2020-11-14 17:43:57 -0600

    Everything works for real

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,33 @@ import "./App.css";
 import styled, { AnyStyledComponent } from "styled-components";
 import ButtonTimer from "./components/Timer";
 
-const Example: AnyStyledComponent = styled.div`
-  color: red;
+const WebsiteHeader: AnyStyledComponent = styled.img`
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+`;
+
+const HeaderText: AnyStyledComponent = styled.div`
+  text-align: center;
+  font-size: 50px;
+`;
+
+const WebsiteFooter: AnyStyledComponent = styled.img`
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
 `;
 
 
 export default () => {
-  
   return (
 	<div>
+	  <WebsiteHeader src='https://media.discordapp.net/attachments/775466871058464799/776985933274152990/unknown.png' />
+	  <HeaderText>Ever wondered your click speed?</HeaderText>
 	  <ButtonTimer />
+	  <WebsiteFooter src='https://media.discordapp.net/attachments/775466871058464799/776988744267988992/unknown.png' />
 	</div>
   );
 };
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -15,9 +15,13 @@ const ResetButton: AnyStyledComponent = styled.img`
 
 const Countdown: AnyStyledComponent = styled.div`
   color: red;
-  font-size: 200px
+  font-size: 100px;
 `;
 
+const GameOverText: AnyStyledComponent = styled.div`
+  color: red;
+  font-size: 80px;
+`;
 
 export default () => {
   const TIMER_LENGTH = 10;
@@ -27,9 +31,9 @@ export default () => {
   const [seconds, setSeconds] = useState(TIMER_LENGTH);
   const [time_out, setTime_out] = useState(false);
   const [counter, setCounter] = useState(0);
-  const	over = <div><ResetButton src='https://media.discordapp.net/attachments/775466871058464799/777009577874685952/unknown.png?width=340&height=671' onClick = {Reset} /><Countdown>Game Over!, your score was: {counter}</Countdown></div>
-  const spam = <div><SpamButton src='https://media.discordapp.net/attachments/775466871058464799/776985933274152990/unknown.png' onClick={SpamClick} /><Countdown>{seconds}</Countdown></div>
-  const begin = <StartButton src='https://media.discordapp.net/attachments/775466871058464799/777019881366224946/unknown.png' onClick = {Start} />
+  const begin = <div><StartButton src='https://media.discordapp.net/attachments/775466871058464799/777310752356433970/unknown.png' onClick = {Start} /><Countdown>Timer: {seconds}s</Countdown></div>
+  const spam = <div><SpamButton src='https://media.discordapp.net/attachments/775466871058464799/777310820614275113/unknown.png' onClick={SpamClick} /><Countdown>Timer: {seconds}s</Countdown></div>
+  const	over = <div><GameOverText>Game Over!<br/>Your score was: {counter}<br/>Your clicking speed is {counter / TIMER_LENGTH} clicks per second</GameOverText><ResetButton src='https://media.discordapp.net/attachments/775466871058464799/777310877241704448/unknown.png' onClick = {Reset} /></div>
   
   function Start(){
     setStarted(true);
